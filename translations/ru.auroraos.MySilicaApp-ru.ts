fd55aa2e3ab5be0f80dbaee54010a4d56db1a4c6
Vladimir Fursov <vvf1717@yandex.ru>
2023-10-07 19:07:14 +0300

    v 0.1

---
+++ translations/ru.auroraos.MySilicaApp-ru.ts
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru">
+<context>
+    <name>DefaultCoverPage</name>
+    <message>
+        <location filename="../qml/cover/DefaultCoverPage.qml" line="46"/>
+        <source>MySilicaApp</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Registration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="53"/>
+        <location filename="../qml/pages/MainPage.qml" line="60"/>
+        <source>Login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="61"/>
+        <source>Enter login</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>Enter your phone number</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="67"/>
+        <source>Phone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="76"/>
+        <source>+7XXXXXXXXXX</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="87"/>
+        <source>Register</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>RegistrationPage</name>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="57"/>
+        <source>New User</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="63"/>
+        <location filename="../qml/pages/RegistrationPage.qml" line="68"/>
+        <source>First name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="74"/>
+        <location filename="../qml/pages/RegistrationPage.qml" line="79"/>
+        <source>Second name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="85"/>
+        <source>Thrid name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="90"/>
+        <source>Third name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="96"/>
+        <location filename="../qml/pages/RegistrationPage.qml" line="101"/>
+        <source>Emale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="108"/>
+        <source>Favorite topics</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/RegistrationPage.qml" line="143"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SuccessPage</name>
+    <message>
+        <location filename="../qml/pages/SuccessPage.qml" line="21"/>
+        <source>OК</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
